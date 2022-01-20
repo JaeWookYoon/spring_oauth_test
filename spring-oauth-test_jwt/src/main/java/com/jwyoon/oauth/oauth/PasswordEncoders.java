dcda2feb083ee3e86c5682718fcd1996acb0564b
iuh12 <iuh12@10.1.97.180>
2022-01-20 15:26:17 +0900

    인코딩 매치로 변경

--- spring-oauth-test_jwt/src/main/java/com/jwyoon/oauth/oauth/PasswordEncoders.java
+++ spring-oauth-test_jwt/src/main/java/com/jwyoon/oauth/oauth/PasswordEncoders.java
@@ -9,13 +9,13 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class PasswordEncoders implements PasswordEncoder {
-	private PasswordEncoder passwordEncoder;
+	private BCryptPasswordEncoder passwordEncoder;
 
 	public PasswordEncoders() {
 		this.passwordEncoder = new BCryptPasswordEncoder();
 	}
 
-	public PasswordEncoders(PasswordEncoder passwordEncoder) {
+	public PasswordEncoders(BCryptPasswordEncoder passwordEncoder) {
 		this.passwordEncoder = passwordEncoder;
 	}
 
@@ -26,17 +26,21 @@ public class PasswordEncoders implements PasswordEncoder {
 
 	@Override
 	public boolean matches(CharSequence rawPassword, String encodedPassword) {
-		System.out.println(rawPassword + " PASSWORD = " + encodedPassword);
-		return rawPassword.equals(encodedPassword);
+		//System.out.println(rawPassword + " PASSWORD = " + encodedPassword);
+		return passwordEncoder.matches(rawPassword,encodedPassword);
 	}
 
 	public static void main(String[] args) {
 		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 		String s = "client:{noop}1234";
+		String a = "1234";
+		String b = "{noop}1234";
+		System.out.println(passwordEncoder.encode(a));
+		System.out.println(passwordEncoder.encode(b));
 		byte[] encodedAuth = Base64.encodeBase64(s.getBytes(StandardCharsets.ISO_8859_1));
         String authHeader = "Basic " + new String(encodedAuth);
 		//System.out.println(passwordEncoder.encode(s));
         System.out.println(authHeader);
 	}
-
+	//$2a$10$QNUU6chh.WnLV0/QStE6YeoMfWSUkW6FZHYicAKNu9D5Kfy/1NjNi
 }

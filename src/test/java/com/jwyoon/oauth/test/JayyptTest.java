9a10cd66670e2a3155a06aad6b41a9cf505a117c
재욱 <iuh12@naver.com>
2022-09-29 19:45:00 +0900

    wallet 쳐내

--- src/test/java/com/jwyoon/oauth/test/JayyptTest.java
+++ src/test/java/com/jwyoon/oauth/test/JayyptTest.java
@@ -5,19 +5,28 @@ import org.junit.Test;
 
 public class JayyptTest {
 
-	/*
-	 * @Test public void contextLoads() { }
-	 * 
-	 * @Test public void jasypt() { String url =
-	 * "jdbc:oracle:thin:@jakedbdb_high?TNS_ADMIN=/src/main/resources/wallet";
-	 * 
-	 * System.out.println(jasyptEncoding(url));
-	 * //System.out.println(jasyptEncoding(username));
-	 * //System.out.println(jasyptEncoding(password)); }
-	 * 
-	 * public String jasyptEncoding(String value) { StandardPBEStringEncryptor
-	 * pbeEnc = new StandardPBEStringEncryptor();
-	 * pbeEnc.setAlgorithm("PBEWithMD5AndDES"); //pbeEnc.setPassword(key); return
-	 * pbeEnc.encrypt(value); }
-	 */
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test 
+	public void jasypt() { 
+		String url = "jdbc:oracle:thin:@(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.ca-toronto-1.oraclecloud.com))(connect_data=(service_name=gcaa070203a63c7_jakedbdb_high.adb.oraclecloud.com))(security=(ssl_server_cert_dn=\"CN=adb.ca-toronto-1.oraclecloud.com, OU=Oracle BMCS TORONTO, O=Oracle Corporation, L=Redwood City, ST=California, C=US\")))";
+				
+		System.out.println(jasyptEncoding(url));
+		
+	  
+	  //System.out.println(jasyptEncoding(username));
+	  //System.out.println(jasyptEncoding(password)); }
+	}
+	public String jasyptEncoding(String value) {
+
+        String key = "jwyoon0717secret_key";
+	
+		StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+		pbeEnc.setAlgorithm("PBEWithMD5AndDES"); //
+		pbeEnc.setPassword(key); 
+		return pbeEnc.encrypt(value);
+	}
+
 }

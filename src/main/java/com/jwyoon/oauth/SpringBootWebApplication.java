a64e86dc15b917983db94b6a94755f4b8b1adc87
재욱 <iuh12@naver.com>
2022-09-29 15:16:11 +0900

    docker image생성완료

--- src/main/java/com/jwyoon/oauth/SpringBootWebApplication.java
+++ src/main/java/com/jwyoon/oauth/SpringBootWebApplication.java
@@ -3,10 +3,11 @@ package com.jwyoon.oauth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 //http://www.thymeleaf.org/doc/articles/layouts.html
 @EnableAutoConfiguration
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class SpringBootWebApplication {
 
 	public static void main(String[] args) throws Exception {
--- src/test/java/com/jwyoon/oauth/test/JayyptTest.java
+++ src/test/java/com/jwyoon/oauth/test/JayyptTest.java
@@ -5,24 +5,19 @@ import org.junit.Test;
 
 public class JayyptTest {
 
-	@Test
-	public void contextLoads() {
-    }
-
-    @Test
-    public void jasypt() {
-        String url = "jdbc:oracle:thin:@jakedbdb_high?TNS_ADMIN=/src/main/resources/wallet";
-        
-        System.out.println(jasyptEncoding(url));
-        //System.out.println(jasyptEncoding(username));
-        //System.out.println(jasyptEncoding(password));
-    }
-
-    public String jasyptEncoding(String value) {
-    	
-        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
-        pbeEnc.setAlgorithm("PBEWithMD5AndDES");
-        //pbeEnc.setPassword(key);
-        return pbeEnc.encrypt(value);
-    }
+	/*
+	 * @Test public void contextLoads() { }
+	 * 
+	 * @Test public void jasypt() { String url =
+	 * "jdbc:oracle:thin:@jakedbdb_high?TNS_ADMIN=/src/main/resources/wallet";
+	 * 
+	 * System.out.println(jasyptEncoding(url));
+	 * //System.out.println(jasyptEncoding(username));
+	 * //System.out.println(jasyptEncoding(password)); }
+	 * 
+	 * public String jasyptEncoding(String value) { StandardPBEStringEncryptor
+	 * pbeEnc = new StandardPBEStringEncryptor();
+	 * pbeEnc.setAlgorithm("PBEWithMD5AndDES"); //pbeEnc.setPassword(key); return
+	 * pbeEnc.encrypt(value); }
+	 */
 }
